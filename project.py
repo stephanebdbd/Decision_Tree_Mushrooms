18b3cab4b1ff6144a95a7a73468606f4f0ddc90b
TBKNbr1 <stephane.badu.budu@ulb.be>
2024-05-01 18:19:26 +0200

    load_datatest

--- project.py
+++ project.py
@@ -3,15 +3,19 @@ import csv
 class Mushroom:
     def __init__(self, edible: bool):
         self.__edible = edible
+        self.__attributes = {'edible': 'Yes' if edible else 'No'}
     
     def is_edible(self):
         return self.__edible
     
     def add_attribute(self, name: str, value: str):
-        return
+        self.__attributes[name] = value
     
     def get_attribute(self, name: str):
-        return
+        return self.__attributes[name]
+
+    def __str__(self):
+        return f'Mushroom(edible={self.__edible}, attributes={self.__attributes})'
 
 class Node:
     def __init__(self, criterion: str, is_leaf: bool=False):
@@ -24,4 +28,15 @@ class Node:
         return
     
 def load_dataset(path: str):
-    return
+    mushrooms = []
+    with open(path, 'r') as file:
+        data = list(csv.reader(file))
+        attributes = data[0]
+        for row in data[1:]:
+            mushroom = Mushroom(row[0] == 'Yes')
+            for attribute, value in zip(attributes[1:], row[1:]):
+                mushroom.add_attribute(attribute, value)
+            mushrooms.append(mushroom)
+    return mushrooms
+
+load_dataset('mushrooms.csv')
