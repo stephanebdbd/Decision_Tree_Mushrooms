28e9136f9a82c90c8e14a91b41015b374c31663c
TBKNbr1 <stephane.badu.budu@ulb.be>
2024-05-07 23:25:12 +0200

    fin

--- project.py
+++ project.py
@@ -2,63 +2,79 @@ import sys, csv
 from math import log2
 
 class Mushroom:
+    # Classe qui représente un champignon
     def __init__(self, edible: bool):
         self.__edible = edible
         self.__attributes = {}
     
     def is_edible(self):
+    # Méthode qui retourne un booléen sur la comestibilité d'un champignon
         return self.__edible
     
     def add_attribute(self, name: str, value: str):
+    # Méthode qui ajoute un attribut à un champignon en le reliant à sa valeur
         self.__attributes[name] = value
     
     def get_attribute(self, name: str):
+    # Méthode qui retourne la valeur d'un attribut d'un champignon
         return self.__attributes[name]
 
     def get_attributes(self):
+    # Méthode qui retourne une liste des attributs d'un champignon
         return list(self.__attributes.keys())
 
 class Node:
+# Classe qui représente un noeud d'un arbre de décision
     def __init__(self, criterion: str, is_leaf: bool=False):
         self.edges_ = []
         self.criterion_ = criterion
         self.is_leaf_ = is_leaf
     
     def get_criterion(self):
+    # Méthode qui retourne le critère du noeud
         return self.criterion_
 
     def is_leaf(self):
+    # Méthode qui retourne un booléen sur la nature du noeud
         return self.is_leaf_
     
     def add_edge(self, label: str, child: "Node"):
+    # Méthode qui ajoute une arête à un noeud
         self.edges_.append(Edge(self, child, label))
 
 class Edge:
+# Classe qui représente une arête d'un arbre de décision
     def __init__(self, parent: Node, child: Node, label: str):
         self.parent_: Node = parent
         self.child_: Node = child
         self.label_: str = label
     
     def get_label(self):
+    # Méthode qui retourne le label de l'arête
         return self.label_
 
     def get_child(self):
+    # Méthode qui retourne le noeud enfant de l'arête
         return self.child_
     
     def get_parent(self):
+    # Méthode qui retourne le noeud parent de l'arête
         return self.parent_
 
 class BooleanTree:
+# Classe qui stocke les données nécessaires pour créer un arbre booléen
     def __init__(self, node: Node, tab: int=0):
         self.node, self.tab = node, tab
         self.edges_ = [edge for edge in node.edges_ if edge.get_child().criterion_ != "No"]
         self.amount_or, self.edge = 0, None
     
     def get_edge_child(self):
+    # Méthode qui retourne le noeud enfant de l'arête stocké
         edge = self.edge
         return edge.get_child()
 
 class PythonScript:
+    # Classe qui stocke les données nécessaires pour créer un script python
     def __init__(self, node:Node, tab: int=1):
         self.leaves, self.node = [], node
         self.tab = tab
@@ -66,183 +82,215 @@ class PythonScript:
         self.edges_ = node.edges_
         
     def clear_leaves(self):
+    # Méthode qui vide la liste des feuilles
         self.leaves.clear()
     
     def edge_get_child(self):
+    # Méthode qui retourne le noeud enfant de l'arête stocké
         edge = self.edge
         return edge.get_child()
     
     def leaves_append(self):
+    # Méthode qui ajoute une feuille à la liste des feuilles
         label = self.edge.get_label()
         self.leaves.append(label)
     
 def load_dataset(path: str):
-    mushrooms = []
-    with open(path, "r", encoding='UTF-8') as file:
+# Fonction qui charge un dataset de champignons
+    mushrooms = [] # Liste qui stocke les champignons
+    with open(path, "r", encoding='UTF-8') as file: # Ouverture du fichier
         data = list(csv.reader(file))
-        attributes = data[0][1:]
-        for row in data[1:]:
-            mushroom = Mushroom(row[0] == "Yes")
+        attributes = data[0][1:] # Liste des attributs
+        for row in data[1:]: # Parcours des lignes du fichier
+            mushroom = Mushroom(row[0] == "Yes") # Création d'un champignon
             for attribute, value in zip(attributes, row[1:]):
-                mushroom.add_attribute(attribute, value)
+                mushroom.add_attribute(attribute, value) # Ajout des attributs
             mushrooms.append(mushroom)
     return mushrooms
 
 def entropy(mushrooms: list[Mushroom]):
+# Fonction qui calcule l'entropie d'un ensemble de champignons
     pY = sum([mushroom.is_edible() for mushroom in mushrooms]) / len(mushrooms)
-    if pY == 0 or pY == 1:
-        return 0
-    return log2(1 - pY)*(pY - 1) - log2(pY)*pY
+    if pY == 0 or pY == 1: # Si la proportion de champignons comestibles est nulle ou égale à 1
+        return 0 # L'entropie est nulle
+    return log2(1 - pY)*(pY - 1) - log2(pY)*pY # Calcul de l'entropie
 
 def get_sub_attributes(mushrooms: list[Mushroom], attribute: str):
-    sub_attributes = set()
-    for mushroom in mushrooms:
-        sub_attributes.add(mushroom.get_attribute(attribute))
+# Fonction qui retourne les sous-attributs d'un attribut
+    sub_attributes = set() # Ensemble qui stocke les sous-attributs
+    for mushroom in mushrooms: # Parcours des champignons
+        sub_attributes.add(mushroom.get_attribute(attribute)) # Ajout de l'attribut
     return sub_attributes
 
 def Ca_v(mushrooms : list[Mushroom], attribute : str):
-    sub_attributes = get_sub_attributes(mushrooms, attribute)
-    res = {sub_attribute: [] for sub_attribute in sub_attributes}
+    # Fonction qui retourne les sous ensembles de champignons pour un attribut par rapport à ses valeurs
+    sub_attributes = get_sub_attributes(mushrooms, attribute) # Récupération des sous attributs
+    res = {sub_attribute: [] for sub_attribute in sub_attributes} # Dictionnaire qui stocke les sous ensembles
     for mushroom in mushrooms:
-        res[mushroom.get_attribute(attribute)].append(mushroom)
+        res[mushroom.get_attribute(attribute)].append(mushroom) # Ajout du champignon dans le sous ensemble
     return res
 
 def get_information_gain(mushrooms: list[Mushroom], attribute : str):
+    # Fonction qui retourne le gain d'information d'un attribut
     h, h1, ca_v = entropy(mushrooms), 0, Ca_v(mushrooms, attribute)
-    for c in ca_v.values():
+    for c in ca_v.values(): # Parcours des sous ensembles
         pa_v = len(c) / len(mushrooms)
-        h2 = entropy(c)
-        h1 += pa_v * h2
-    return h - h1
+        h2 = entropy(c) # Calcul de l'entropie
+        h1 += pa_v * h2 # Calcul pour chaque sous ensemble
+    return h - h1 # Calcul du gain d'information
 
 def get_best_attribute(mushrooms: list[Mushroom]):
+    # Fonction qui retourne le meilleur attribut pour construire l'arbre de décision
     attributes = mushrooms[0].get_attributes()
     best_attribute = attributes[0]
     best_information_gain = get_information_gain(mushrooms, best_attribute)
-    for attribute in attributes[1:]:
-        information_gain = get_information_gain(mushrooms, attribute)
-        if information_gain > best_information_gain:
-            best_attribute = attribute
-            best_information_gain = information_gain
+    for attribute in attributes[1:]: # Parcours des attributs
+        information_gain = get_information_gain(mushrooms, attribute) # Calcul du gain d'information
+        if information_gain > best_information_gain: # Si le gain d'information est supérieur
+            best_attribute = attribute # Mise à jour de l'attribut
+            best_information_gain = information_gain # Mise à jour du gain d'information
     return best_attribute
 
 def build_decision_tree(mushrooms: list[Mushroom]):
-    best_attribute = get_best_attribute(mushrooms)
-    r = Node(best_attribute)
-    ca_v = Ca_v(mushrooms, best_attribute)
-    for sub_attribute, c in ca_v.items():
-        if entropy(c) == 0:
+    # Fonction qui construit un arbre de décision
+    best_attribute = get_best_attribute(mushrooms) # Récupération du meilleur attribut
+    r = Node(best_attribute) # Création du noeud ayant comme racine le meilleur attribut
+    ca_v = Ca_v(mushrooms, best_attribute) # Récupération des sous ensembles
+    for sub_attribute, c in ca_v.items(): # Parcours des sous ensembles
+        if entropy(c) == 0: # Si l'entropie est nulle
             criterion = "Yes" if c[0].is_edible() else "No"
-            r.add_edge(sub_attribute, Node(criterion, True))
+            r.add_edge(sub_attribute, Node(criterion, True)) # Création d'une feuille
         else:
-            r.add_edge(sub_attribute, build_decision_tree(c))
+            r.add_edge(sub_attribute, build_decision_tree(c)) # Création d'un noeud via récursivité
     return r
 
-def display(tree: Node):
-    def display_r(node: Node, tab: int=0, tree_str:str=""):
-        for edge in node.edges_:
-            tree_str += "    "*tab + f"{node.criterion_} = {edge.get_label()}\n"
-            child = edge.get_child()
-            if not child.is_leaf():
-                tree_str += display_r(child, tab+1)
-            else:
-                tree_str += "    "*(tab+1) + child.criterion_ + "\n"
-        return tree_str
-    tree_str = display_r(tree)
-    print(tree_str)
-    return tree_str
-
 def is_edible(root: Node, mushroom: Mushroom):
-    while not root.is_leaf():
+    # Fonction qui retourne un booléen sur la comestibilité d'un champignon
+    while not root.is_leaf(): # Tant que le noeud n'est pas une feuille
         n, i = len(root.edges_), 0
         finished = False
-        while i < n and not finished:
+        while i < n and not finished: # Parcours des arêtes
             edge = root.edges_[i]
-            if mushroom.get_attribute(root.criterion_) == edge.get_label():
-                root = edge.get_child()
+            if mushroom.get_attribute(root.criterion_) == edge.get_label(): # Si l'attribut est égal au label
+                root = edge.get_child() # Mise à jour du noeud
                 finished = True
             i += 1
-    return root.criterion_ == "Yes"
+    return root.criterion_ == "Yes" # Retourne un booléen sur la comestibilité
+
+def display(tree: Node):
+    # Fonction qui affiche un arbre de décision
+    def display_r(node: Node, tab: int=0, tree_str:str=""):
+        # Fonction récursive qui affiche un arbre de décision
+        for edge in node.edges_:  # Parcours des arêtes
+            tree_str += "    "*tab + f"{node.criterion_} = {edge.get_label()}\n"
+            # Affichage du critère et du label de l'arête
+            child = edge.get_child()
+            if not child.is_leaf(): # Si le noeud n'est pas une feuille
+                tree_str += display_r(child, tab+1) # Appel récursif
+            else:
+                tree_str += "    "*(tab+1) + child.criterion_ + "\n"
+                # Affichage de la comestibilité d'feuille
+        return tree_str
+    tree_str = display_r(tree) # Affichage de l'arbre
+    print(tree_str) # Affichage de l'arbre
+    return tree_str
 
 def print_not_leaf(bt: BooleanTree, tree: str=""):
+    # Fonction qui affiche un noeud non feuille
     node, edges, tab = bt.node, bt.edges_, bt.tab
     amount_or, child, edge = bt.amount_or, bt.get_edge_child(), bt.edge
     par = len([i.get_child().criterion_ != "No" for i in child.edges_]) > 1
-    tree += f"({node.criterion_} = {edge.get_label()}"
-    tree += " AND (\n" if par else " AND \n"
-    tree += boolean_tree_r(BooleanTree(child, tab+1))
+    # Récupération des données nécessaires via la classe BooleanTree
+    tree += f"({node.criterion_} = {edge.get_label()}" # Affichage du critère et du label de l'arête
+    tree += " AND (\n" if par else " AND \n" # Affichage du "AND" si le noeud a plusieurs enfants
+    tree += boolean_tree_r(BooleanTree(child, tab+1)) # Appel récursif
     tree += (")" if par else "") + (") OR " if amount_or < len(edges) else ")")
-    tree += "\n" + "    "*tab
+    # Affichage de la parenthèse fermante et du "OR" si le noeud a plusieurs enfants
+    tree += "\n" + "    "*tab # Retour à la ligne et indentation
     return tree
 
 def print_leaf(bt: BooleanTree, tree: str=""):
+    # Fonction qui affiche une feuille
     node, edges, tab = bt.node, bt.edges_, bt.tab
     amount_or, edge = bt.amount_or, bt.edge
-    tree += f"({node.criterion_} = {edge.get_label()})"
-    tree += " OR " if amount_or < len(edges) else ""
+    # Récupération des données nécessaires via la classe BooleanTree
+    tree += f"({node.criterion_} = {edge.get_label()})" # Affichage du critère et du label de l'arête
+    tree += " OR " if amount_or < len(edges) else "" # Affichage du "OR" si le noeud a plusieurs enfants
     n = amount_or < len(edges)-1 and edges[amount_or].get_child().criterion_ == "Yes"
     tree += "\n" + "    "*tab if n and amount_or % 2 == 0 else ""
+    # Retour à la ligne et indentation pour renre l'affichage plus lisible
     return tree
 
 def boolean_tree_r(bt: BooleanTree, tree: str=""):
-    tree += "    "*bt.tab
-    for bt.amount_or, bt.edge in enumerate(bt.edges_):
+    # Fonction récursive qui affiche un arbre booléen
+    tree += "    "*bt.tab # Indentation
+    for bt.amount_or, bt.edge in enumerate(bt.edges_): # Parcours des arêtes
         bt.amount_or += 1
         child = bt.get_edge_child()
-        if not child.is_leaf():
-            tree += print_not_leaf(bt)
+        if not child.is_leaf(): # Si ce n'est pas une feuille
+            tree += print_not_leaf(bt) # Appel à la fonction qui print une arête
         elif child.criterion_ == "Yes":
-            tree += print_leaf(bt)
+            tree += print_leaf(bt) # Appel à la fonction qui print une feuille
     return tree
 
 def boolean_tree(root: Node):
+    # Fonction qui crée et renvoie un arbre booléen
     tree = "(" + boolean_tree_r(BooleanTree(root)) + ")"
+    # Appel à la fonction récursive créatrice de l'arbre booléen
     print(tree)
     return tree
 
 def print_criterions(ps: PythonScript, script: str=""):
+    # Fonction qui print les critères des feuilles
     tab, node, leaves, if_ = ps.tab, ps.node, ps.leaves, ps.if_
     criterions = False
-    if len(leaves) > 3:
+    # Récupération des données nécessaires via la classe PythonScript
+    if len(leaves) > 3: # Si le nombre de feuilles est supérieur à 3
         script += "    "*tab + f"criterions = {leaves}\n"
-        criterions = True
+        criterions = True # On met les critères dans une liste
     script += "    "*tab + ("elif" if if_ and not criterions else "if")
     ps.if_, if_ = True, True
-    if len(leaves) == 1:
+    if len(leaves) == 1: # Si le nombre de feuilles est égal à 1
         script += f" mushroom.get_attribute('{node.criterion_}') == '{leaves[0]}':\n"
-    elif len(leaves) > 0:
+    elif len(leaves) > 0: # Si le nombre de feuilles est supérieur à 0, on regarde si on a mis les critères dans la liste
         script += f" mushroom.get_attribute('{node.criterion_}')"
         script += " in criterions:\n" if criterions else f" in {leaves}:\n"
         ps.clear_leaves()
     script += "    "*(tab+1) + "return True\n"
     return script
 
-def print_if_leaves(ps: PythonScript, script: str=""):
+def print_if_not_leaves(ps: PythonScript, script: str=""):
+    # Fonction qui print la condition si le noeud n'est pas une feuille
     tab, node, edge, if_ = ps.tab, ps.node, ps.edge, ps.if_
-    script += "    "*tab + ("elif" if if_ else "if")
+    script += "    "*tab + ("elif" if if_ else "if") # Affichage du "elif" ou du "if"
     script += f" mushroom.get_attribute('{node.criterion_}') == '{edge.get_label()}':\n"
     return script
 
 def to_python_r(ps: PythonScript, script: str=""):
-    for edge in ps.edges_:
+    # Fonction récursive qui crée un script python
+    for edge in ps.edges_: # Parcours des arêtes
         ps.edge = edge
         child = ps.edge_get_child()
-        if not child.is_leaf():
-            script += print_if_leaves(ps)
-            script += to_python_r(PythonScript(child, ps.tab+1))
+        if not child.is_leaf(): # Si le noeud n'est pas une feuille
+            script += print_if_not_leaves(ps) # Appel à la fonction qui print la condition
+            script += to_python_r(PythonScript(child, ps.tab+1)) # Appel récursif
         elif child.criterion_ == 'Yes':
-                ps.leaves_append()
-    script += print_criterions(ps)
+                ps.leaves_append() # Ajout d'une feuille dans la liste
+    script += print_criterions(ps) # Appel à la fonction qui print les valeurs des feuilles comesibles
     return script
 
 def to_python(dt: Node, path: str):
+    # Fonction qui crée un script python
     script = "from project import *\n\ndef is_edible(mushroom: Mushroom):\n"
-    script += to_python_r(PythonScript(dt))
+    # Importation du fichier et création de la fonction
+    script += to_python_r(PythonScript(dt)) # Appel à la fonction récursive
     script += "    else:\n" + "    "*(2) + "return False"
-    with open(path, "w") as file:
-        file.write(script)
+    # Ajout de la condition pour les feuilles non comestibles
+    with open(path, "w") as file: # Ouverture du fichier
+        file.write(script) # Écriture du script
 
 def main(argv, argc):
+    # Fonction principale "test"
     path1 = "mushrooms.csv" if argc < 2 else argv[1]
     path2 = "decision_tree.py" if argc < 3 else argv[2]
     mushrooms = load_dataset(path1)
--- tests.py
+++ tests.py
@@ -46,81 +46,95 @@ class TestBuildTree(unittest.TestCase):
         self.assertFalse(is_edible(root, make_mushroom({'odor': 'None', 'spore-print-color': 'Green'})))
 
 class PersonnalTestsPart1(unittest.TestCase):
+# Classe contenant les tests de la 1ère partie du projet
+# Les tests sont effectués sur les fonctions de calcul d'entropie et d'information gain
     def setUp(self):
         self.mushrooms = load_dataset('mushrooms.csv')
         self.test_tree_root = build_decision_tree(self.mushrooms)
     
     def test_entropy(self):
+    # Test de la fonction d'entropie
         edible = [m for m in self.mushrooms if m.is_edible()]
         poisonous = [m for m in self.mushrooms if not m.is_edible()]
-        self.assertEqual(entropy(edible), 0)
-        self.assertEqual(entropy(poisonous), 0)
+        self.assertEqual(entropy(edible), 0) # Entropie de la liste des champignons comestibles
+        self.assertEqual(entropy(poisonous), 0) # Entropie de la liste des champignons non comestibles
         self.assertEqual(entropy(self.mushrooms), 0.9990678968724604)
+        # Entropie de la liste des champignons
     
     def test_information_gain(self):
+    # Test de la fonction d'information gain
         tree = self.test_tree_root
-        self.assertEqual(tree.criterion_, get_best_attribute(self.mushrooms))
-        self.assertEqual(get_information_gain(self.mushrooms, 'odor'), 0.9060749773839998)
+        self.assertEqual(tree.criterion_, get_best_attribute(self.mushrooms)) # Le critère de division des champignons
+        self.assertEqual(get_information_gain(self.mushrooms, 'odor'), 0.9060749773839998) # Information gain pour le critère 'odor'
         mushrooms = [m for m in self.mushrooms if m.get_attribute('odor') == 'None']
         tree_e = [e for e in tree.edges_ if e.get_label() == "None"][0]
         tree = tree_e.child_
-        self.assertEqual(tree.criterion_, get_best_attribute(mushrooms))
-        self.assertEqual(get_information_gain(mushrooms, 'spore-print-color'), 0.14493721491485229)
+        self.assertEqual(tree.criterion_, get_best_attribute(mushrooms)) # Le critère de division après la division par la valeur 'None' de 'odor'
+        self.assertEqual(get_information_gain(mushrooms, 'spore-print-color'), 0.14493721491485229) # Information gain pour le critère 'spore-print-color'
 
 class PersonnalTestsPart2(unittest.TestCase):
+# Classe contenant les tests de la 2ème partie du projet
+# Les tests sont effectués sur les fonctions d'affichage de l'arbre de décision et de génération de code Python
     def setUp(self):
         self.mushrooms = load_dataset('mushrooms.csv')
         self.test_tree_root = build_decision_tree(self.mushrooms)
         self.path = "decision_tree.py"
     
     def test_is_edible(self):
+    # Test de la fonction is_edible
         root = self.test_tree_root
-        self.assertTrue(is_edible(root, make_mushroom({'odor': 'Almond'})))
-        self.assertFalse(is_edible(root, make_mushroom({'odor': 'None', 'spore-print-color': 'Green'})))
+        self.assertTrue(is_edible(root, make_mushroom({'odor': 'Almond'}))) # Champignon comestible
+        self.assertFalse(is_edible(root, make_mushroom({'odor': 'None', 'spore-print-color': 'Green'}))) # Champignon non comestible
     
     def test_display(self):
+    # Test de la fonction d'affichage de l'arbre de décision
         tree_d = display(self.test_tree_root).split()
-        tree_d = [t.strip() for t in tree_d if "=" not in t]
+        tree_d = [t.strip() for t in tree_d if "=" not in t] # Listes des attributs et valeurs affichés
         tree = self.test_tree_root
-        self.assertEqual(tree.criterion_, "odor")
-        stack = {tree.criterion_ : [(edge, edge.get_parent()) for edge in tree.edges_]}
-        while sum(len(l) for l in stack.values()) > 0:
-            criterion, label = tree_d.pop(0), tree_d.pop(0)
-            edge, node = stack[criterion].pop(0)
-            if tree_d[0] in ["No", "Yes"]:
+        self.assertEqual(tree.criterion_, "odor") # Le critère de division de l'arbre
+        stack = {tree.criterion_ : [(edge, edge.get_parent()) for edge in tree.edges_]} # Dictionnaire des listes des arêtes et des nœuds parents en fonction des attributs
+        while sum(len(l) for l in stack.values()) > 0: # Tant qu'il reste des éléments dans le dictionnaire
+            criterion, label = tree_d.pop(0), tree_d.pop(0) # Récupération de l'attribut et de la valeur affichés
+            edge, node = stack[criterion].pop(0) # Récupération de la première arête et du premier nœud parent
+            if tree_d[0] in ["No", "Yes"]: # Si le nœud est une feuille
                 node = edge.get_child()
-                criterion = tree_d.pop(0)
-                self.assertTrue(node.is_leaf())
-            self.assertEqual(node.criterion_, criterion)
-            self.assertEqual(edge.get_label(), label)
-            if not node.is_leaf():
-                self.modify_stack(stack, edge)
+                criterion = tree_d.pop(0) # Récupération de la valeur de la feuille
+                self.assertTrue(node.is_leaf()) # Le nœud doit être une feuille
+            self.assertEqual(node.criterion_, criterion) # Le nœud doit avoir le même attribut que celui affiché
+            self.assertEqual(edge.get_label(), label) # L'arête doit avoir la même valeur que celle affichée
+            if not node.is_leaf(): # Si le nœud n'est pas une feuille
+                self.modify_stack(stack, edge) # Mise à jour du dictionnaire
 
     def modify_stack(self, stack, edge):
-        node = edge.get_child()
-        suite = [(e, e.get_parent()) for e in node.edges_]
-        if len(suite) > 0:
-            next_c = suite[0][-1].criterion_
-            if not next_c in stack:
-                stack[next_c] = suite
+    # Fonction de mise à jour du dictionnaire des arêtes et des nœuds parents
+        node = edge.get_child() # Récupération du nœud enfant
+        suite = [(e, e.get_parent()) for e in node.edges_] # Liste des arêtes et des nœuds parents
+        if len(suite) > 0: # Si la liste n'est pas vide
+            next_c = suite[0][-1].criterion_ # Récupération de l'attribut du premier nœud parent
+            if not next_c in stack: 
+                stack[next_c] = suite # Ajout de la liste au dictionnaire
             else:
-                stack[next_c].extend(suite)
+                stack[next_c].extend(suite) # Ajout des éléments de la liste au dictionnaire
             
     def test_to_python(self):
-        to_python(self.test_tree_root, self.path)
-        from decision_tree import is_edible
-        self.assertTrue(is_edible(make_mushroom({'odor': 'Almond'})))
-        self.assertFalse(is_edible(make_mushroom({'odor': 'None', 'spore-print-color': 'Green'})))
+    # Test de la fonction de génération de code Python
+        to_python(self.test_tree_root, self.path) # Génération du code Python
+        from decision_tree import is_edible # Importation de la fonction is_edible
+        self.assertTrue(is_edible(make_mushroom({'odor': 'Almond'}))) # Champignon comestible
+        self.assertFalse(is_edible(make_mushroom({'odor': 'None', 'spore-print-color': 'Green'}))) # Champignon non comestible
 
 class TestBooleanTree(unittest.TestCase):
+# Classe contenant les tests de la génération de l'arbre booléen
     def setUp(self):
         self.mushrooms = load_dataset('mushrooms.csv')
         self.tree = build_decision_tree(self.mushrooms)
         self.boolean_tree = boolean_tree(self.tree)
     
     def test_count_parenthesis(self):
+        # Test du nombre de parenthèses
         tree = self.boolean_tree
-        self.assertEqual(tree.count("("), tree.count(")"))
+        self.assertEqual(tree.count("("), tree.count(")")) 
+        # Test du nombre de parenthèses pour 3 arbres différents
         
         tree2 = build_decision_tree(self.mushrooms)
         bt2 = boolean_tree(tree2)
@@ -131,30 +145,36 @@ class TestBooleanTree(unittest.TestCase):
         self.assertEqual(bt3.count("("), bt3.count(")"))
     
     def test_boolean_tree(self):
+        # Test de l'arbre booléen
         tree = self.tree
         tree_bt = self.boolean_tree
         tree_bt = tree_bt.replace("(", "").replace(")", "")
         tree_bt = tree_bt.split()
         tree_bt = [t.strip() for t in tree_bt if t.strip() not in ["OR", "AND", "="]]
+        # Listes des éléments de l'arbre booléen
         self.assertEqual(tree.criterion_, "odor")
         stack = {tree.criterion_ : [(edge, edge.get_parent()) for edge in tree.edges_ if edge.get_child().criterion_ != "No"]}
-        while sum(len(l) for l in stack.values()) > 0:
+        # Dictionnaire des listes des arêtes et des nœuds parents en fonction des attributs
+        while sum(len(value) for value in stack.values()) > 0: # Tant qu'il reste des éléments dans le dictionnaire
             criterion, label = tree_bt.pop(0), tree_bt.pop(0)
             edge, node = stack[criterion].pop(0)
-            self.assertEqual(node.criterion_, criterion)
-            self.assertEqual(edge.get_label(), label)
-            if not node.is_leaf():
-                self.modify_stack(stack, edge)
+            # Récupération de l'attribut et de la valeur affichés, de la première arête et du premier nœud parent
+            self.assertEqual(node.criterion_, criterion) # Le nœud doit avoir le même attribut que celui affiché
+            self.assertEqual(edge.get_label(), label) # L'arête doit avoir la même valeur que celle affichée
+            if not node.is_leaf(): # Si le nœud n'est pas une feuille
+                self.modify_stack(stack, edge) # Mise à jour du dictionnaire
     
     def modify_stack(self, stack, edge):
-        node = edge.get_child()
+    # Fonction de mise à jour du dictionnaire des arêtes et des nœuds parents
+        node = edge.get_child() # Récupération du nœud enfant
         suite = [(e, e.get_parent()) for e in node.edges_ if e.get_child().criterion_ != "No"]
-        if len(suite) > 0:
-            next_c = suite[0][-1].criterion_
-            if not next_c in stack:
-                stack[next_c] = suite
+        # Liste des arêtes et des nœuds parents
+        if len(suite) > 0: # Si la liste n'est pas vide
+            next_c = suite[0][-1].criterion_ # Récupération de l'attribut du premier nœud parent
+            if not next_c in stack: # Si l'attribut n'est pas dans le dictionnaire
+                stack[next_c] = suite # Ajout de la liste au dictionnaire
             else:
-                stack[next_c].extend(suite)
+                stack[next_c].extend(suite) # Ajout des éléments de la liste au dictionnaire
 
 if __name__ == '__main__':
     unittest.main()
